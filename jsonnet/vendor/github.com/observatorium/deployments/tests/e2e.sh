a33f4082ff2ed3b5a59574de4cc635b8b39c55a7
Song Song Li <ssli@redhat.com>
2021-12-08 14:48:54 +0800

    use kube-prom release-0.9 for test
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- jsonnet/vendor/github.com/observatorium/deployments/tests/e2e.sh
+++ jsonnet/vendor/github.com/observatorium/deployments/tests/e2e.sh
@@ -26,8 +26,8 @@ dex() {
 }
 
 deploy() {
-    $KUBECTL apply -f https://raw.githubusercontent.com/prometheus-operator/kube-prometheus/main/manifests/setup/0servicemonitorCustomResourceDefinition.yaml
-    $KUBECTL apply -f https://raw.githubusercontent.com/prometheus-operator/kube-prometheus/main/manifests/setup/0prometheusruleCustomResourceDefinition.yaml
+    $KUBECTL apply -f https://raw.githubusercontent.com/prometheus-operator/kube-prometheus/release-0.9/manifests/setup/0servicemonitorCustomResourceDefinition.yaml
+    $KUBECTL apply -f https://raw.githubusercontent.com/prometheus-operator/kube-prometheus/release-0.9/manifests/setup/0prometheusruleCustomResourceDefinition.yaml
     $KUBECTL create ns observatorium-minio || true
     $KUBECTL create ns observatorium || true
     dex
